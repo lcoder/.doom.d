af6e5c80459be1a4c2c550e85279d7391d62ade2
leo <leo@moego.pet>
2025-09-29 12:02:50 +0800

    feat: rust format

--- config.el
+++ config.el
@@ -238,6 +238,12 @@
   :config
   (setq org-roam-directory (expand-file-name "roam")))
 
+;; rust下的格式化
+(after! apheleia
+  (set-formatter! 'rustfmt
+    '("rustfmt" "--edition" "2024")
+    :modes '(rust-mode rustic-mode rust-ts-mode)))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
